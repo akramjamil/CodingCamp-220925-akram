f9796f3ba8cd451bbb6ccb24a1b63701f705b2b7
akramjamil <akramjamil18@gmail.com>
2025-09-27 06:27:40 +0700

    perbaiki script

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-// Greeting
 window.onload = function () {
-  let name = prompt("Masukkan nama Anda:");
-  if (name) {
-    document.getElementById("welcome").innerText = `Hi ${name}, Welcome To Our Website`;
+  
+  let userNamePrompt = prompt("Masukkan nama Anda:");
+  if (userNamePrompt) {
+    document.getElementById("welcome").innerText = `Hi ${userNamePrompt}, Welcome To Noir Studio!`;
   }
+
+  
+  const form = document.getElementById("messageForm");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formName = document.getElementById("name").value;
+    const dob = document.getElementById("dob").value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const message = document.getElementById("messageText").value;
+
+    
+    const now = new Date();
+
+    document.getElementById("time").innerText = now.toString();
+    document.getElementById("resName").innerText = formName;
+    document.getElementById("resDob").innerText = dob;
+    document.getElementById("resGender").innerText = gender;
+    document.getElementById("resMessage").innerText = message;
+
+    
+    form.reset();
+  });
 };
-
-// Message Form
-document.getElementById("messageForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const dob = document.getElementById("dob").value;
-  const gender = document.querySelector('input[name="gender"]:checked').value;
-  const message = document.getElementById("messageText").value;
-
-  // Tampilkan waktu sekarang
-  const now = new Date();
-
-  document.getElementById("time").innerText = now.toString();
-  document.getElementById("resName").innerText = name;
-  document.getElementById("resDob").innerText = dob;
-  document.getElementById("resGender").innerText = gender;
-  document.getElementById("resMessage").innerText = message;
-
-  // Reset form
-  document.getElementById("messageForm").reset();
-});
